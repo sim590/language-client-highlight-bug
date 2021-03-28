7db0fc7d1e7a3ea4888fc89b5d190defab6a9962
Simon Désaulniers <sim.desaulniers@gmail.com>
2021-03-28 02:33:24 -0400

    main: minimal example

---
+++ app/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+my_func :: Int -> Int
+my_func = undefined
+
+myint :: Int
+myint = 5
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
+
+--  vim: set sts=2 ts=2 sw=2 tw=120 et :
